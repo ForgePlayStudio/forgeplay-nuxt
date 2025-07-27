4593ab974b25dbed7c291a6bce35f119f0856cc6
StrongAle <alessio.falchi90@gmail.com>
2025-07-27 19:46:06 +0100

    project start

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite"
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    'motion-v/nuxt'
+  ],
+  css: [
+    '@/assets/css/main.css',
+  ],
+})
